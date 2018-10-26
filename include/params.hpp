083674980a2c8b3b0a57d37ef6c4c345f5949c1c
Piotr Nycz <piotr.nycz@nokia.com>
2018-10-26 18:56:36 +0200

    not work for ref

--- include/params.hpp
+++ include/params.hpp
@@ -1,11 +1,54 @@
 #pragma once
 
-namespace np
+#include <utility>
+#include <array>
+
+
+namespace named
 {
 
+template <typename name, typename T>
+struct arg;
 
+template <typename name, typename T>
+struct param
+{
+    template <typename U>
+    param(arg<name, U> const& a_);
+
+    operator T() const { return value; }
+    T value;
+};
+
+template <typename name, typename T>
+struct arg
+{
+    T value;
+};
+
+
+
+template <typename name>
+struct evaluator
+{
+    template <typename T>
+    constexpr auto operator = (T&& value) const &
+    {
+        return arg<name, T&&>{std::forward<T>(value) };
+    }
+};
+
+template <typename name>
+constexpr evaluator<name> p_;
+
+
+
+template <typename name, typename T>
+template <typename U>
+param<name, T>::param(arg<name, U> const& a_)
+    : value(a_.value)
+{}
 
 
 }
 
-#define P_(name) ::np::p_(#name)
--- tests/named_params_tests.cpp
+++ tests/named_params_tests.cpp
@@ -1,1 +1,65 @@
 #include <params.hpp>
+#include <gtest/gtest.h>
+#include <string>
+#include <sstream>
+#include <algorithm>
+#include <iterator>
+
+
+struct x;
+struct y;
+struct n;
+struct str;
+struct ref;
+struct value;
+
+static int passThrough(named::param<x, int> x)
+{
+    return x;
+}
+static int add(named::param<x, int> x, named::param<y, int> y)
+{
+    return x + y;
+}
+static std::string repeatStr(named::param<n, int> n, named::param<str, std::string> str)
+{
+    std::ostringstream res;
+    std::fill_n(std::ostream_iterator<std::string>(res, ""), n, str);
+    return res.str();
+}
+
+static void replaceValue(named::param<ref, std::string&> ref, named::param<value, std::string const&> value)
+{
+    ref = value;
+}
+
+using namespace ::testing;
+struct NamedParamTest : public Test
+{};
+
+using named::p_;
+
+TEST_F(NamedParamTest, canUseOne)
+{
+    ASSERT_EQ(7, passThrough(p_<x> = 7));
+    ASSERT_EQ(13, passThrough(p_<x> = 13));
+}
+
+TEST_F(NamedParamTest, canUseMoreThanOne)
+{
+    ASSERT_EQ(20, add(p_<x> = 7, p_<y> = 13));
+    ASSERT_EQ(0, add(p_<x> = 7, p_<y> = -7));
+}
+
+TEST_F(NamedParamTest, canUseMoreThanOneType)
+{
+    using namespace std::string_literals;
+    ASSERT_EQ("abc abc abc "s, repeatStr(p_<n> = 3, p_<str> = "abc "s));
+}
+
+TEST_F(NamedParamTest, canUseReference)
+{
+    std::string str = "First";
+    replaceValue(p_<ref> = str, p_<value> = "Second");
+    ASSERT_EQ(str, "Second");
+}
